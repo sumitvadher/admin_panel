55d18d7fc5f60c5e8d8a33f6a8c7e95ad6ed5c78
Sumit Vadher <4464636+sumitvadher@users.noreply.github.com>
2025-03-11 19:15:35 +0300

    Delete src directory

--- src/app/mapping/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function DashboardPage() {
-  return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold">Mapping</h1>
-      <p>Welcome to the Admin Panel Dashboard.</p>
-    </div>
-  );
-}
